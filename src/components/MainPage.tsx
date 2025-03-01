8ae3f648afdae0b03627e16e732bc08fdc6b887a
Gordon06060 <gordonz0913@gmail.com>
2025-03-01 14:16:29 -0500

    feat: Navbar

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeroHighlightDemo } from "./HeroHighlightDemo";
+
+export default function MainPage() {
+  return (
+    <>
+      <HeroHighlightDemo />
+    </>
+  );
+}
